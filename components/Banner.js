68bb598a85333540b76a48e034e0ed466566b83f
Rajeeva Mahalinga <rajeeva@telosalliance.com>
2020-06-12 21:55:54 -0400

    first commit

---
+++ components/Banner.js
@@ -0,0 +1,18 @@
+const Banner = (props) => (
+    <section id="banner" className="major">
+        <div className="inner">
+            <header className="major">
+                <h1>Hi, Sowjanya Prabhu</h1>
+            </header>
+            <div className="content">
+                <p>Cooking has been a spiritual journey for me, the process truly brings me the joy and the calm.
+                    I am here to share the love of cooking and a pinch of myself.</p>
+                <ul className="actions">
+                    <li><a href="#one" className="button next scrolly">Get Started</a></li>
+                </ul>
+            </div>
+        </div>
+    </section>
+)
+
+export default Banner
